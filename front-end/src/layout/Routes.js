5dbea73a22bdb23346b1e82901be31d1ccee2b8e
Chris Pagan <christopher.pagan@outlook.com>
2022-01-26 03:56:40 -0800

    both front and back end tests passing

---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import ReservationCreate from "../Reservations/Create"
+import TableCreate from "../Tables/Create";
+import NotFound from "./NotFound";
+import Seat from "../Reservations/Seat";
+import Search from "../Search/Search";
+import Edit from "../Reservations/Edit";
+import { today } from "../utils/date-time";
+
+/**
+ * Defines all the routes for the application.
+ *
+ * You will need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <Seat />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <Edit />
+      </Route>
+      <Route path="/reservations/new">
+        <ReservationCreate />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard date={today()} />
+      </Route>
+      <Route path="/tables/new">
+        <TableCreate />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
